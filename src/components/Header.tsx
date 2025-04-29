9661fc333f32eb501a61028c615c58f6d1c88924
hessnt30 <hessnt30@vt.edu>
2025-04-29 08:42:50 -0400

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex w-full bg-white h-12 items-center p-6">
+      <h1 className="text-black text-2xl font-bold">SportsHub</h1>
+    </div>
+  );
+}
---
+++ src/components/Videos.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const YOUTUBE_PLAYLIST_ITEMS_API =
+  "https://www.googleapis.com/youtube/v3/playlistItems";
+
+const PLAYLIST_ID = "PLL-lmlkrmJanq-c41voXY4cCbxVR0bjxR";
+
+type Video = {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: {
+        url: string;
+        width: number;
+        height: number;
+      };
+      medium: {
+        url: string;
+        width: number;
+        height: number;
+      };
+      high: {
+        url: string;
+        width: number;
+        height: number;
+      };
+      standard: {
+        url: string;
+        width: number;
+        height: number;
+      };
+    };
+    channelTitle: string;
+    playlistId: string;
+    position: number;
+    resourceId: {
+      kind: string;
+      videoId: string;
+    };
+    videoOwnerChannelTitle: string;
+    videoOwnerChannelId: string;
+  };
+};
+
+export default function Video() {
+  const [videos, setVideos] = useState<Video[] | null>();
+
+  useEffect(() => {
+    /**
+     * Initial fetch of videos from MLB 2025 Game Highlights playlist
+     */
+    const fetchVideos = async () => {
+      const res = await fetch(
+        `${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&maxResults=100&playlistId=${PLAYLIST_ID}&key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`
+      );
+
+      const data = await res.json();
+      findTeamVideos("Nationals", data.items as Video[]);
+    };
+
+    fetchVideos();
+  }, []);
+
+  useEffect(() => {
+    console.log("data", videos);
+  }, [videos]);
+
+  /**
+   * Filter videos by looking for `team` in title and update `videos` state.
+   * @param team Team name to filter results by
+   * @param vids All videos from playlist
+   * @returns `null` if `vids` is null
+   */
+  function findTeamVideos(team: string, vids: Video[]) {
+    if (!vids) return null;
+    const filteredVids = vids.filter((video) => {
+      return video.snippet.title.includes(team);
+    });
+    console.log("vids:", filteredVids);
+    setVideos(filteredVids);
+  }
+
+  if (!videos) return <p className="p-6">Loading...</p>;
+  return (
+    <div className="p-6">
+      <h1>Game Recaps</h1>
+      <ul className="grid grid-cols-1 gap-2 md:grid-cols-2 max-w-3xl">
+        {videos.map((video) => {
+          return (
+            <li key={video.id}>
+              <div className="flex p-4 border border-gray-800 rounded-md max-w-sm">
+                <a
+                  href={`https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}`}
+                  target="_blank"
+                >
+                  <p>
+                    <Image
+                      width={video.snippet.thumbnails.medium.width}
+                      height={video.snippet.thumbnails.medium.height}
+                      src={video.snippet.thumbnails.medium.url}
+                      alt={video.snippet.title}
+                    />
+                  </p>
+                  <h3 className="">
+                    {video.snippet.title.replace(" | MLB Highlights", "")}
+                  </h3>
+                </a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
